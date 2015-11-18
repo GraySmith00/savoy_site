6a94324f9d1bd1b1c232e22d52fa558a4c47a70b
Gray Smith <gray.smith00@gmail.com>
2015-11-18 01:39:39 +0000

    version 2, restyle and strip down site

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,21 +1,21 @@
-$(function() {
-  // Cache the window object
-  var $window = $(window);
+// $(function() {
+//   // Cache the window object
+//   var $window = $(window);
   
-  //parallax background effect
-  $('section[data-type="background"]').each(function(){
-    var $bgobj = $(this);
-    $(window).scroll(function(){
-      // scroll the background at var speed
-      // the yPos is a negative value because we're scrolling it up!
-      var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+//   //parallax background effect
+//   $('section[data-type="background"]').each(function(){
+//     var $bgobj = $(this);
+//     $(window).scroll(function(){
+//       // scroll the background at var speed
+//       // the yPos is a negative value because we're scrolling it up!
+//       var yPos = -($window.scrollTop() / $bgobj.data('speed'));
       
-      // Put together our final background position
-      var coords = '50% '+ yPos + 'px';
+//       // Put together our final background position
+//       var coords = '50% '+ yPos + 'px';
       
-      // Move the background
-      $bgobj.css({ backgroundPosition: coords });
+//       // Move the background
+//       $bgobj.css({ backgroundPosition: coords });
       
-    }); // end window scroll
-  })
-});
+//     }); // end window scroll
+//   })
+// });
